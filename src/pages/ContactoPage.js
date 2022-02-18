09765f7ebd291270f0f54a806c302f9dfac32c12
Alejandro Cancelos <alcancelos@gmail.com>
2022-02-17 22:29:00 -0300

    contacto

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -1,32 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/components/pages/ContactoPage.css';
+import axios from 'axios';
+
 
 const ContactoPage = (props) => {
+    
+
+    const initialForm ={
+        name:'',
+        email:'',
+        subject:'Jurídico',
+        message:''
+    }
+
+   
+
+    const [sending,setSending]=useState(false);
+    const [msg, setMsg]=useState('');
+    const [formData,setFormData] = useState(initialForm);
+
+    const handleChange = e =>{
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value //forma dinamica
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response=await axios.post('https://opjuridicocontable-backend.herokuapp.com/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if(response.data.error===false){
+            setFormData(initialForm)
+        }
+     }
+    
+
     return (
         <main>
             <div className="formulario">
                 <h3>Contacto</h3>
-                <form method="post" action="">
+                <form onSubmit={handleSubmit}>
                     <div>
-                        <label for="name">Nombre: </label>
-                        <input type="text" id="name" name="name" value="" placeholder="Su nombre" required="required"
-                            autofocus />
+                        <label htmlfor="name">Nombre: </label>
+                        <input value={formData.name} onChange={handleChange}  type="text" id="name" name="name" placeholder="Su nombre"  required
+                         />
                     </div>
                     <div>
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" value="" placeholder="your@email.com" required="required" />
+                        <label htmlfor="email">Email:</label>
+                        <input value={formData.email} onChange={handleChange} type="email" name="email" placeholder="your@email.com" required />
                     </div>
                     <div>
-                        <label for="subject">Motivo:</label>
-                        <select id="subject" name="subject">
-                            <option value="hello">Jurídico</option>
-                            <option value="quote">Contable</option>
+                        <label htmlfor="subject">Motivo:</label>
+                        <select id="subject" name="subject" value={formData.subject} onChange={handleChange} >
+                            <option value="Juridico"  >Jurídico</option>
+                            <option value="Contable"  >Contable</option>
                         </select>
+                        
                     </div>
                     <div>
-                        <label for="message">Comentarios:</label>
-                        <textarea id="message" name="message" placeholder="Escriba sus comentarios aqui." required></textarea>
+                        <label htmlfor="message">Comentarios:</label>
+                        <textarea id="message" name="message" placeholder="Escriba sus comentarios aqui." required value={formData.message} onChange={handleChange}></textarea>
                     </div>
+                    {sending ? <p>Enviando...</p> : null}
+                    {msg ? <p>{msg}</p> : null}
                     <div className="boton">
                         <input type="submit" value="enviar" className="btn-enviar" />
                     </div>
@@ -42,7 +83,7 @@ const ContactoPage = (props) => {
                     <p>Dirección: Colombres 23 4A</p>
                 </div>
 
-
+ 
             </div>
         </main>
     );
